7ab32881c95239e51f500110af1bada445a45a7d
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2016-06-16 16:20:13 -0700

    Added pry-byebug and awesome print gems to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'pry' if ENV['APP_ENV'] == 'debug' # add `binding.pry` wherever you need to debug
+require 'pry-byebug'
+require 'awesome_print'
 
 require_relative '../war'
 require_relative '../player'
