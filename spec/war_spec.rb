4de9cc4c5cec5c214ef8e2bea5317a3a905ab69b
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2016-06-20 16:23:31 -0700

    Revised War and specs

--- spec/war_spec.rb
+++ spec/war_spec.rb
@@ -2,21 +2,12 @@ require 'spec_helper'
 
 RSpec.describe War do
   describe 'play' do
-    let(:num_players) {
-      {
-        num_players: 3
-      }
-    }
-
-    it 'plays the game' do
-      expect{
-        War.new(num_players).play
-      }.to_not raise_error
+    it 'plays the game with a number of players between 2 and 4 as input' do
+      (2..4).each do |i|
+        expect{
+          War.new({num_players: i})
+        }.to_not raise_error
+      end
     end
-
-    it 'accepts a number of players between 2 and 4 as input'
-    it 'displays the outcome of each battle as output when the battle ends'
-    it 'displays the outcome of each war as output when the war ends'
-    it 'displays the winner of the game as output when the game ends'
   end
 end
--- war.rb
+++ war.rb
@@ -1,81 +1,91 @@
 require File.expand_path(File.dirname(__FILE__) + '/player')
+require File.expand_path(File.dirname(__FILE__) + '/deck')
+require 'pry'
+require 'pry-byebug'
 
 class War
-  attr_reader :deck, :deck_count, :num_players, :players, :total_cards_in_play
-  attr_accessor :winner, :round_winner, :current_score, :score_board
+  attr_reader :deck_count, :players, :total_cards_in_play
+  attr_accessor :num_players, :deck, :round_winner, :score_board
 
   def initialize(args)
-    @num_players = args.fetch(:num_players)
+    @num_players = args.fetch(:num_players, 2)
     @players = setup_players
-    @deck = shuffle_deck
-    @deck_count = deck.count
+    @deck = Deck.new
+    @deck_count = deck.cards.count
     @total_cards_in_play = num_players*(deck_count/num_players)
-    @current_score = initialize_current_score
     @score_board = initialize_score_board
     @round_winner = nil
-    @winner = nil
   end
 
   def play
+    shuffle_deck
     deal_cards
-    unless winner
-      draw
-      if war?(current_score)
-        puts 'War on!'
-        double_draw
-      else
-        player = round_winner
-        puts "The round winner is #{player.object_id}"
-        player.take_all(score_board.values.flatten)
-        initialize_score_board
-        initialize_current_score
-      end
+    loop do
       winner = game_over?
+      if winner
+        puts "Game Over! The winner is #{players.first.name}"
+        break
+      else
+        draw
+        if war?
+          puts 'War on!'
+          draw
+        else
+          player = round_winner
+          player.take_all(score_board.values.flatten)
+          puts "The round winner is #{player.name}, with score: #{winning_score}"
+          @score_board = initialize_score_board
+        end
+      end
     end
-    puts "Game Over! The winner is #{winner}"
   end
 
   private
 
   def round_winner
-    find_player_by(current_score.max.first)
+    id = nil
+    score_board.each_pair{|k,v| id = k if v.last.rank == winning_score}
+    find_player_by(id)
   end
 
-  def war?(current_score)
-    case num_players
+  def winning_score
+    score_board.values.map{|v| v.last.rank}.max
+  end
+
+  def war?
+    scores = score_board.values.map{|v| v.last.rank if v.last.rank}
+
+    case scores.count
     when 2
-      return current_score.values.uniq.count == 1
+      return scores.uniq.count == 1
     when 3
-      return current_score.values.uniq.count == 1 || current_score.values.uniq.count == 2
+      return scores.uniq.count == 1 || scores.uniq.count == 2
     when 4
-      return current_score.values.uniq.count == 1 || current_score.values.uniq.count == 2 || current_score.values.uniq.count == 3
+      return scores.uniq.count == 1 || scores.uniq.count == 2 || scores.uniq.count == 3
     else
       return false
     end
   end
 
   def game_over?
-    players.each do |player|
-      return player if player.cards.count == total_cards_in_play
-    end
-
-    false
-  end
-
-  def update_score
-    score_board.merge(current_score){|key, first, second| first << second }
+    players.count == 1
   end
 
   def draw
     players.each do |player|
-      current_score[player.object_id] = player.draw
+      if player.cards.any?
+        score_board[player.object_id] << player.draw
+      else
+        drop_out(player)
+      end
     end
-    update_score
   end
 
-  def double_draw
-    draw
-    draw
+  def drop_out(player)
+    id = player.object_id
+    players.delete(player)
+    score_board.delete(id)
+    @num_players -= 1
   end
 
   def setup_players
@@ -86,14 +96,14 @@ class War
   end
 
   def shuffle_deck
-    deck_of_cards.flatten.shuffle!(random: srand)
+    deck.shuffle
   end
 
   def deal_cards
     num_cards_to_deal = deck_count/num_players
     (1..num_cards_to_deal).each do |i|
       players.each do |p|
-        p.cards << deck.pop
+        p.cards << deck.cards.pop
       end
     end
   end
@@ -105,26 +115,10 @@ class War
     player
   end
 
-  def initialize_current_score
-    h = {}
-    players.map{|p| h[p.object_id] = []}
-
-    h
-  end
-
   def initialize_score_board
     h = {}
     players.map{|p| h[p.object_id] = []}
 
     h
-  end
-
-  def deck_of_cards
-    [
-      %w(A K Q J T 9 8 7 6 5 4 3 2),
-      %w(A K Q J T 9 8 7 6 5 4 3 2),
-      %w(A K Q J T 9 8 7 6 5 4 3 2),
-      %w(A K Q J T 9 8 7 6 5 4 3 2)
-    ]
   end
 end
